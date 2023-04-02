d5832741f6afd5328180c381706257c97bc8b0ad
humbertomcar <humbertomcarneiro2004@gmail.com>
2023-04-02 20:05:29 -0300

    programa final sem herança

--- Atividade9/Bank.java
+++ Atividade9/Bank.java
@@ -1,51 +1,67 @@
 package Atividade9;
+import java.util.Scanner;
 
 public class Bank {
-    protected int acc;
-    protected double amount;
-    protected double dinheiroSacado;
-    protected double dinheiroDepositado;
-    protected double newAmount;
-
-    public Bank(){
+    public enum operacoes{
+        Depositar, 
+        Sacar,
+        Conta,
+        Abrir
     }
-    public Bank(int acc, double amount, double dinheiroDepositado, double dinheiroSacado
-    , double newAmount){
-        this.acc = acc;
-        this.amount = amount;
-        this.dinheiroDepositado = dinheiroDepositado;
-        this.dinheiroSacado = dinheiroSacado;
-        this.newAmount = newAmount;
-    }
-    public double getAcc() {
-        return acc;
-    }
-    public double depositar(double amount, double dinheiroDepositado, double newAmount){
-        if(newAmount != 0) {
-            newAmount = newAmount + dinheiroDepositado;
-        }
-        newAmount = amount + dinheiroDepositado;
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        double valor;
+        double saldo = 0;
+        int acc = 0;
+        int criar = 0;
+        int agenda = 0;
+        int swit = 0;
         
-        return newAmount;
-    }
-    public double sacar(double amount, double dinheiroSacado, double newAmount){
-        if(newAmount != 0) {
-            newAmount = newAmount - dinheiroSacado;
-        }else{
-            newAmount = amount - dinheiroSacado;
+        System.out.println("Insira a ação desejada:");
+        System.out.println("1 - Depositar | 2 - Sacar | 3 - Registrar");
+        while (agenda == 0){
+            criar = s.nextInt();
+            if(criar == 1){
+                System.out.print("Quanto você deseja depositar? ");
+                valor = s.nextDouble();
+                saldo += valor;
+                swit = 2;
+            }
+            if(criar == 2){
+                System.out.print("Quanto você deseja sacar? ");
+                valor = s.nextDouble();
+                if(valor > saldo){
+                    System.out.println("Dinheiro insuficiente!");
+                }else{
+                    saldo -= valor;
+                    swit = 2;
+                }
+            }
+            if(criar == 3){
+                System.out.print("Digite sua conta: ");
+                acc = s.nextInt();
+                swit = 1;
+            }
+            if(criar == 4){
+                System.out.println("Você saiu");
+                break;
+            }
+            switch(swit){
+               
+                case 1:
+                operacoes conta = operacoes.Conta;
+                System.out.println("Sua conta é : " + acc);
+                break;
+
+                case 2:
+                operacoes quantia = operacoes.Abrir;
+                System.out.println("Saldo atual: " + saldo);
+                break;
+            }
+            System.out.println("Insira a ação desejada:");
+            System.out.println("1 - Depositar | 2 - Sacar | 3 - Registrar | 4 - Sair");
+            
         }
-        if(newAmount <= 0){
-            throw new RuntimeException("Você está liso!");
-        }
-        return newAmount;
-    }
-    public double getAmount() {
-        return amount;
-    }
-    public void setAmount(double amount) {
-        this.amount = amount;
-    }
-    public double getNewAmount(){
-        return newAmount;
     }
 }
+
